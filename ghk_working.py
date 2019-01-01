c58c10c80f3c227ddc592eabf748acd49ec543df
Leona Mähler <39636591+pocafavilla@users.noreply.github.com>
2019-01-01 19:37:29 +0100

    Update ghk_working.py

--- ghk_working.py
+++ ghk_working.py
@@ -1,43 +1,5 @@
 from fractions import Fraction
 import numpy as np
-#print(2*Fraction(1,4)+Fraction(1,4))
-
-"""K = 310
-
-KO = 4
-KI = 140
-NaO = 150
-NaI = 15
-ClO = 0
-ClI = 0
-
-K = 310
-
-PK = 1
-PNa = 0.333333333
-PCl = 0
-
-
-
-
-RT = 8.3144598 * K
-F = 9.6485*10*10*10*10
-
-#print(RT/F)
-up = PK*KO+PNa*NaO+PCl*ClI
-down = PK*KI+PNa*NaI+PCl*ClO
-print(up)
-print(down)
-print(up/down)
-res = RT/F * (np.log(up/down))
-#res = 58 * np.log2(Fraction(PK*KO,PK*KI)+Fraction(PNa*NaO,PNa*NaI))
-#res = RT/ F * np.log2(PK*Fraction(KO/KI)+PNa*Fraction(NaO/NaI+PCl*Fraction(ClI/ClO)))
-#res = RT/ F * np.log2(PK*Fraction(KO/KI)+PNa*Fraction(NaO/NaI+PCl*Fraction(ClI/ClO)))
-
-
-print("{} Volt".format(res))
-
-#f(x) = RT/F * ln(PKo/PKi+Nao/Nai+Cli/Clo)"""
 
 def ghk_equation (K = 310, KO = 4, KI = 140, NaO = 150, NaI = 15, ClO = 0, ClI = 0, PK = 1, PNa = 0.333333333, PCl = 0):
 
